82e491171b9938a6b59503c5ac5095642df122b8
xuxiaocheng <2762267080@qq.com>
2024-05-20 22:52:51 +0800

    feat(lib): 添加更多ffi

--- src/ffi.rs
+++ src/ffi.rs
@@ -7,6 +7,17 @@ use rusqlite::{Error, ffi, Result};
 
 extern "C" {
     pub fn sqlite3_simple_init(db: *mut ffi::sqlite3, pz_err_msg: *mut *const c_char, p_api: *const ffi::sqlite3_api_routines) -> c_int;
+
+
+    pub fn simple_query(ctx: *mut ffi::sqlite3_context, argc: c_int, argv: *mut *mut ffi::sqlite3_value);
+
+    #[cfg(feature = "jieba")]
+    #[cfg_attr(docsrs, doc(cfg(feature = "jieba")))]
+    pub fn jieba_dict(ctx: *mut ffi::sqlite3_context, argc: c_int, argv: *mut *mut ffi::sqlite3_value);
+
+    #[cfg(feature = "jieba")]
+    #[cfg_attr(docsrs, doc(cfg(feature = "jieba")))]
+    pub fn jieba_query(ctx: *mut ffi::sqlite3_context, argc: c_int, argv: *mut *mut ffi::sqlite3_value);
 }
 
 /// This is a re-exported and enhanced version of [`rusqlite::error::check(res)`](rusqlite::error::check)
