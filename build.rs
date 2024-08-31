33b278beb1bc256b9b97053c152183229aa165ca
xuxiaocheng <2762267080@qq.com>
2024-08-31 17:54:08 +0800

    feat(lib): 优化构建脚本

--- build.rs
+++ build.rs
@@ -1,18 +1,11 @@
-use std::env;
-
 fn main() {
-    let out_dir = env::var("OUT_DIR").unwrap();
-
     let mut cfg = cc::Build::new();
 
     cfg.include("simple/src");
-    cfg.file("simple/src/pinyin.h");
-    cfg.file("simple/src/simple_highlight.h");
-    cfg.file("simple/src/simple_tokenizer.h");
+    cfg.file("simple/src/entry.cc");
     cfg.file("simple/src/pinyin.cc");
     cfg.file("simple/src/simple_highlight.cc");
     cfg.file("simple/src/simple_tokenizer.cc");
-    cfg.file("simple/src/entry.cc");
 
     cfg.include("simple/contrib/sqlite3");
 
@@ -21,16 +14,18 @@ fn main() {
     cfg.file("cmrc/pinyin.txt/pinyin.txt.cpp");
 
     if cfg!(feature = "jieba") {
-        cfg
-            .define("USE_JIEBA", "1")
-            .include("cppjieba/include")
-            .include("cppjieba/deps/limonp/include");
+        cfg.define("USE_JIEBA", "1");
+        cfg.include("cppjieba/include");
+        cfg.include("cppjieba/deps/limonp/include");
     }
 
-    cfg
-        .cpp(true)
-        .std("c++14")
-        .flag_if_supported("/utf-8");
+    cfg.flag("-Wno-comment");
+    cfg.flag("-Wno-unused-parameter");
+    cfg.flag("-Wno-deprecated-declarations");
+    cfg.flag("-Wno-unused-but-set-variable");
+
+    cfg.cpp(true);
+    cfg.std("c++14");
+    cfg.flag_if_supported("/utf-8");
     cfg.compile("simple");
-    println!("cargo:lib_dir={out_dir}");
 }
