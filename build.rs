64eb0a86d0b3595a0012ed45a85162701ff1ec11
XuXiaoCheng <2762267080@qq.com>
2026-03-10 03:56:08 +0800

    fix(lib): 手动执行 cmake

--- build.rs
+++ build.rs
@@ -4,19 +4,52 @@ fn main() {
 }
 
 fn generate_cmrc() -> std::path::PathBuf {
-    let mut cfg = cmake::Config::new("simple");
+    // https://github.com/rust-lang/cmake-rs/issues/273
+    // let mut cfg = cmake::Config::new("simple");
+    // cfg.define("BUILD_SQLITE3", "off");
+    // cfg.define("SIMPLE_WITH_JIEBA", "off");
+    // cfg.define("BUILD_TEST_EXAMPLE", "off");
+    // cfg.generator("Unix Makefiles");
+    // cfg.build();
 
-    cfg.define("BUILD_SQLITE3", "off");
-    cfg.define("SIMPLE_WITH_JIEBA", "off");
-    cfg.define("BUILD_TEST_EXAMPLE", "off");
+    let mut out_dir = std::path::PathBuf::from(std::env::var("OUT_DIR").unwrap());
+    out_dir.push("build");
+    let out_dir = out_dir;
 
-    if cfg!(windows) {
-        cfg.generator("Unix Makefiles");
+    // https://docs.rs/crate/cmake/0.1.57/source/src/lib.rs#1091-1115
+    fn run(cmd: &mut std::process::Command, program: &str) {
+        eprintln!("running: {:?}", cmd);
+        let status = match cmd.status() {
+            Ok(status) => status,
+            Err(ref e) if e.kind() == std::io::ErrorKind::NotFound =>
+                panic!("failed to execute command: {e}\nis `{program}` not installed?"),
+            Err(e) => panic!("failed to execute command: {e}"),
+        };
+        if !status.success() {
+            if status.code() == Some(127) {
+                panic!("command did not execute successfully, got: {status}, is `{program}` not installed?");
+            }
+            panic!("command did not execute successfully, got: {status}");
+        }
     }
 
-    let mut path = cfg.build();
-    path.push("build");
-    path
+    run(
+        std::process::Command::new(std::env::var("CMAKE").unwrap_or("cmake".to_string()))
+            .arg("simple")
+            .arg("-B").arg(&out_dir)
+            .arg("-DBUILD_SQLITE3=off")
+            .arg("-DSIMPLE_WITH_JIEBA=off")
+            .arg("-DBUILD_TEST_EXAMPLE=off")
+            .arg("-G").arg("Unix Makefiles"),
+        "cmake",
+    );
+    run(
+        std::process::Command::new(std::env::var("MAKE").unwrap_or("make".to_string()))
+            .current_dir(&out_dir),
+        "make",
+    );
+
+    out_dir
 }
 
 fn compile_simple(cmrc_dir: std::path::PathBuf) {
@@ -30,9 +63,9 @@ fn compile_simple(cmrc_dir: std::path::PathBuf) {
 
     cfg.include("simple/contrib/sqlite3");
 
-    cfg.include(&cmrc_dir.join("_cmrc/include"));
-    cfg.file(&cmrc_dir.join("__cmrc_PINYIN_TEXT/lib.cpp"));
-    cfg.file(&cmrc_dir.join("__cmrc_PINYIN_TEXT/intermediate/contrib/pinyin.txt.cpp"));
+    cfg.include(cmrc_dir.join("_cmrc/include"));
+    cfg.file(cmrc_dir.join("__cmrc_PINYIN_TEXT/lib.cpp"));
+    cfg.file(cmrc_dir.join("__cmrc_PINYIN_TEXT/intermediate/contrib/pinyin.txt.cpp"));
 
     if cfg!(feature = "jieba") {
         cfg.define("USE_JIEBA", "1");
